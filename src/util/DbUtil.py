df9c6f8b56536159d6646809d2a78d4d2e0c7eac
dyz <903251743@qq.com>
2023-11-15 11:59:40 +0800

    添加辅助模块类DbUtil

---
+++ src/util/DbUtil.py
@@ -0,0 +1,24 @@
+import pymysql
+
+
+# 帮助类
+class DbUtil:
+    connect: object
+    cursor: object
+
+    def __init__(self, student, username, password):
+        self.connect = pymysql.Connect(host='localhost', port = 3306, database='studentxj',
+                                       user=username, passwd=password, charset='utf8')
+        self.cursor = self.connect.cursor()
+
+    def close(self):
+        self.connect.close()
+        self.cursor.close()
+
+    def execute(self, sql: str, val: tuple = None):
+        self.cursor.execute(sql, val)
+        self.connect.commit()
+
+    def excuteList(self, sql: str):
+        self.cursor.execute(sql, None)
+        return self.cursor.fetchall()
