5cfb3b176ba7750a62d8c7eab36f47a0316d4dbe
dyz <903251743@qq.com>
2023-11-16 01:34:25 +0800

    完成对po包的添加

--- src/po/book.py
+++ src/po/book.py
@@ -8,4 +8,16 @@ class Book:
     sbir: str
     stele: str
 
-    def __init__(self):
+    def __init__(self, sno: str = None, sname: str = None, ssex: str = None, sclass: str = None, smajor: str = None,
+                 sdept: str = None, sbir: str = None, stele: str = None):
+        self.sno = sno
+        self.sname = sname
+        self.ssex = ssex
+        self.sclass = sclass
+        self.smajor = smajor
+        self.sdept = sdept
+        self.sbir = sbir
+        self.stele = stele
+
+    def __str__(self):
+        return f"{self.sno},{self.sname},{self.ssex},{self.sclass},{self.smajor},{self.sdept},{self.sbir},{self.stele}"
